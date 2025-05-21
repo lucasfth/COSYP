3640bc08f6476e44d259f86bf704ee2ee95ea960
Frederik Petersen <fred.p@live.dk>
2025-05-21 15:24:31 +0200

    Use deno for javascript

--- assignment2/javascript/mandelbrot/mandelbrot.js
+++ assignment2/javascript/mandelbrot/mandelbrot.js
@@ -4,104 +4,100 @@
    http://benchmarksgame.alioth.debian.org/
 
    contributed by Andreas Schmelz 2016-02-14
+   adapted for Deno by GitHub Copilot 2025
 */
 
-const cluster = require('cluster');
-// Prev const numCPUs = require('os').cpus().length * 2;
-var numCPUs = require('os').cpus().length * 2;
-var fs = require('fs');
+/*
+Modified for Deno compatibility and performance improvements.
+*/
 
-const d = parseInt(process.argv[2]) || 200;
+// Get the number of available CPU cores
+const cpuCores = navigator.hardwareConcurrency || 4;
+let numCPUs = cpuCores * 2;
 
+// Parse dimension from command line arguments
+const d = parseInt(Deno.args[0]) || 200;
+
+// Validate input dimension
 if (d % 8 != 0) {
-  console.error('d must be multiple of 8');
-  process.exit(-1);
+  console.error("d must be multiple of 8");
+  Deno.exit(-1);
 }
-// Prev
-// if (d * d / numCPUs % 8 != 0) {
-//   console.error('cannot distribute equal across cpus');
-//   process.exit(-1);
-// }
 
-while (d * d / numCPUs % 8 != 0) {
+// Ensure even distribution of work across CPUs
+while (((d * d) / numCPUs) % 8 != 0) {
   numCPUs--;
 }
 
-if (cluster.isMaster) {
-  var alive = numCPUs;
-  var part_buffer = new Array(numCPUs);
-  for (var i = 0; i < numCPUs; i++) {
-    var worker = cluster.fork();
-    var j = i;
+/**
+ * Main function to run Mandelbrot calculation using multiple processes
+ */
+async function main() {
+  // Output header for PBM format
+  const textEncoder = new TextEncoder();
+  const header = `P4\n${d} ${d}\n`;
+  await Deno.stdout.write(textEncoder.encode(header));
 
-    worker.on('message', function(e) {
-      part_buffer[this.id - 1] = new Buffer(e.data);
-      this.kill();
-      alive--;
-      if (alive == 0) {
-        //var fd = fs.openSync('test3.pbm', 'w');
-        //fs.writeSync(fd, 'P4\n'+d+' '+d+'\n');
-        process.stdout.write('P4\n'+d+' '+d+'\n')
-        for (var i = 0; i < numCPUs; i++) {
-          process.stdout.write(part_buffer[i]);
-          //fs.writeSync(fd, part_buffer[i], 0, part_buffer[i].length);
-        }
-      }
+  const processes = [];
+  const outputs = [];
 
+  // Spawn worker processes for parallel computation
+  for (let i = 0; i < numCPUs; i++) {
+    const start = Math.floor((i * d) / numCPUs);
+    const end = Math.floor(((i + 1) * d) / numCPUs);
+
+    // Run worker process
+    const process = Deno.run({
+      cmd: [
+        "deno",
+        "run",
+        "--allow-read",
+        "--allow-write",
+        "worker.js",
+        d.toString(),
+        start.toString(),
+        end.toString(),
+        (i + 1).toString(),
+      ],
+      stdout: "piped",
+      stderr: "piped",
     });
+
+    processes.push(process);
   }
-} else if (cluster.isWorker) {
-  var id = cluster.worker.id;
-  var start = Math.floor((id - 1) * d / numCPUs), // incl
-      end = Math.floor(id * d / numCPUs);   // excl
 
-  var byte_acc = 0,
-      bit_num = 0,
-      iter = 50,
-      limit = 4;
+  // Collect and process outputs
+  for (const process of processes) {
+    const output = await process.output();
+    await Deno.stdout.write(output);
 
-  //console.log('create buffer with '+(d * d / 8 / numCPUs));
-  var buff = new Buffer(d * d / 8 / numCPUs);
-
-  (function() {
-    var xd = 2 / d;
-    var it = 0;
-    for (var y = start; y < end; y++) {
-      var yd = 2 * y / d - 1;
-      for (var x = 0; x < d; x++) {
-
-        var sum = doCalc(
-          xd * x - 1.5,
-          yd
-        );
-
-        byte_acc |= (sum <= limit);
-        bit_num++;
-
-        if (bit_num === 8) {
-          buff[it++] = byte_acc;
-          byte_acc = 0,
-          bit_num = 0;
-        } else {
-          byte_acc <<= 1;
-        }
-      }
-    }
-  })();
-
-  process.send(buff);
+    // Close the process resources
+    process.close();
+  }
 }
 
+/**
+ * Calculate the mandelbrot set value at a specific point
+ * @param {number} Cr - Real part of complex number
+ * @param {number} Ci - Imaginary part of complex number
+ * @return {number} Squared magnitude
+ */
 function doCalc(Cr, Ci) {
-  var Zr = 0,
-      Zi = 0,
-      Tr = 0,
-      Ti = 0;
-  for (var i = 0; i < iter && Tr + Ti <= limit; i++ ) {
-    Zi = 2 * Zr * Zi + Ci,
-    Zr = Tr - Ti + Cr,
-    Tr = Zr * Zr,
+  let Zr = 0,
+    Zi = 0,
+    Tr = 0,
+    Ti = 0;
+  const iter = 50;
+  const limit = 4;
+
+  for (let i = 0; i < iter && Tr + Ti <= limit; i++) {
+    Zi = 2 * Zr * Zi + Ci;
+    Zr = Tr - Ti + Cr;
+    Tr = Zr * Zr;
     Ti = Zi * Zi;
   }
   return Tr + Ti;
-};
+}
+
+// Start the application
+main();
