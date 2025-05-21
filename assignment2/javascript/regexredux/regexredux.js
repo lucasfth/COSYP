3d79b60a15419d6da27186c9d41c083f84b411cc
Lucas Frey Torres Hanson <lucasfth@bjelke-torres.com>
2025-05-21 16:36:20 +0200

    Fix deno comp

--- assignment2/javascript/regexredux/regexredux.js
+++ assignment2/javascript/regexredux/regexredux.js
@@ -1,35 +1,68 @@
-// https://github.com/greensoftwarelab/Energy-Languages/blob/master/JavaScript/regex-redux/regexredux.node-2.node
-//
 // The Computer Language Benchmarks Game
 // http://benchmarksgame.alioth.debian.org/
 //
 // regex-dna program contributed by Jesse Millikan
 // Base on the Ruby version by jose fco. gonzalez
-// fixed by Matthew Wilson 
+// fixed by Matthew Wilson
 // ported to Node.js and sped up by Roman Pletnev
 // converted from regex-dna program
+// Modified for Deno compatibility
 
-var fs = require('fs'), i = fs.readFileSync('/dev/stdin', 'ascii'),
-  ilen = i.length, clen, j,
-  q = [/agggtaaa|tttaccct/ig, /[cgt]gggtaaa|tttaccc[acg]/ig,
-    /a[act]ggtaaa|tttacc[agt]t/ig, /ag[act]gtaaa|tttac[agt]ct/ig,
-    /agg[act]taaa|ttta[agt]cct/ig, /aggg[acg]aaa|ttt[cgt]ccct/ig,
-    /agggt[cgt]aa|tt[acg]accct/ig, /agggta[cgt]a|t[acg]taccct/ig,
-    /agggtaa[cgt]|[acg]ttaccct/ig],
+// DENO MODIFICATION: Replace Node.js file reading with Deno's API
+async function main() {
+  // Read input data
+  const decoder = new TextDecoder("ascii");
+  const buffer = new Uint8Array(1024 * 1024); // 1MB buffer
+  let i = "";
 
-  b = [
-    "-",    /\|[^|][^|]*\|/g, 
-    "|",   /<[^>]*>/g, 
-    "<2>", /a[NSt]|BY/g, 
-    "<3>", /aND|caN|Ha[DS]|WaS/g, 
-    "<4>", /tHa[Nt]/g  
-    ];
+  while (true) {
+    const readResult = await Deno.stdin.read(buffer);
+    if (readResult === null) break;
+    i += decoder.decode(buffer.subarray(0, readResult), { stream: true });
+  }
 
-i = i.replace(/^>.*\n|\n/mg, '');
-clen = i.length;
-for(j = 0; j<q.length; ++j) {
-  var qj = q[j], m = i.match(qj);
-  console.log(qj.source, m ? m.length : 0);
+  // Original code starts here
+  const ilen = i.length;
+  let clen, j;
+  const q = [
+    /agggtaaa|tttaccct/gi,
+    /[cgt]gggtaaa|tttaccc[acg]/gi,
+    /a[act]ggtaaa|tttacc[agt]t/gi,
+    /ag[act]gtaaa|tttac[agt]ct/gi,
+    /agg[act]taaa|ttta[agt]cct/gi,
+    /aggg[acg]aaa|ttt[cgt]ccct/gi,
+    /agggt[cgt]aa|tt[acg]accct/gi,
+    /agggta[cgt]a|t[acg]taccct/gi,
+    /agggtaa[cgt]|[acg]ttaccct/gi,
+  ];
+
+  const b = [
+    "-",
+    /\|[^|][^|]*\|/g,
+    "|",
+    /<[^>]*>/g,
+    "<2>",
+    /a[NSt]|BY/g,
+    "<3>",
+    /aND|caN|Ha[DS]|WaS/g,
+    "<4>",
+    /tHa[Nt]/g,
+  ];
+
+  i = i.replace(/^>.*\n|\n/gm, "");
+  clen = i.length;
+  for (j = 0; j < q.length; ++j) {
+    const qj = q[j],
+      m = i.match(qj);
+    console.log(qj.source, m ? m.length : 0);
+  }
+
+  while (b.length) i = i.replace(b.pop(), b.pop());
+  console.log(["", ilen, clen, i.length].join("\n"));
 }
-while(b.length) i = i.replace(b.pop(), b.pop());
-console.log(["", ilen, clen, i.length].join("\n"));
+
+// DENO MODIFICATION: Run the main function
+main().catch((err) => {
+  console.error("Error:", err);
+  Deno.exit(1);
+});
--- assignment2/typescript/regexredux/regexredux.ts
+++ assignment2/typescript/regexredux/regexredux.ts
@@ -4,48 +4,77 @@
    http://benchmarksgame.alioth.debian.org/
 
    by Josh Goldfoot, adapted from the node.js version
-   compile with tsc --lib es7 regexredux.ts
+   Modified for Deno compatibility
 */
 
-/// <reference path="../node_modules/@types/node/index.d.ts" />
+// DENO MODIFICATION: Remove Node.js references and imports
+// /// <reference path="../node_modules/@types/node/index.d.ts" />
+// var fs = require("fs");
 
-var fs = require("fs");
+// DENO MODIFICATION: Read from stdin using Deno API
+async function main() {
+  // Read the input data
+  const decoder = new TextDecoder("ascii");
+  const buffer = new Uint8Array(1024 * 1024); // 1MB buffer
+  let i = "";
 
-var i = fs.readFileSync("/dev/stdin", "ascii");
-var ilen = i.length;
-i = i.replace(/^>.*\n|\n/mg, "");
-var clen = i.length;
+  while (true) {
+    const readResult = await Deno.stdin.read(buffer);
+    if (readResult === null) break;
+    i += decoder.decode(buffer.subarray(0, readResult), { stream: true });
+  }
 
-var iubReplaceLen: Promise<number> = new Promise<number>(resolve => {
-    var iub: string[] = ["-", "|", "<2>", "<3>", "<4>"];
-    var iubR: RegExp[] = [/\|[^|][^|]*\|/g, /<[^>]*>/g, /a[NSt]|BY/g, 
-        /aND|caN|Ha[DS]|WaS/g, /tHa[Nt]/g];
-    var seq = i + "";
-    while (iub.length)
-        seq = seq.replace(iubR.pop(), iub.pop())
+  // Process the input
+  const ilen = i.length;
+  i = i.replace(/^>.*\n|\n/gm, "");
+  const clen = i.length;
+
+  const iubReplaceLen: Promise<number> = new Promise<number>((resolve) => {
+    const iub: string[] = ["-", "|", "<2>", "<3>", "<4>"];
+    const iubR: RegExp[] = [
+      /\|[^|][^|]*\|/g,
+      /<[^>]*>/g,
+      /a[NSt]|BY/g,
+      /aND|caN|Ha[DS]|WaS/g,
+      /tHa[Nt]/g,
+    ];
+    let seq = i + "";
+    while (iub.length) seq = seq.replace(iubR.pop()!, iub.pop()!);
     resolve(seq.length);
-});
-    
-var q: RegExp[] = [/agggtaaa|tttaccct/ig, /[cgt]gggtaaa|tttaccc[acg]/ig,
-    /a[act]ggtaaa|tttacc[agt]t/ig, /ag[act]gtaaa|tttac[agt]ct/ig,
-    /agg[act]taaa|ttta[agt]cct/ig, /aggg[acg]aaa|ttt[cgt]ccct/ig,
-    /agggt[cgt]aa|tt[acg]accct/ig, /agggta[cgt]a|t[acg]taccct/ig,
-    /agggtaa[cgt]|[acg]ttaccct/ig];
+  });
 
-var promises: Promise<string>[] = q.map(r => new Promise<string>(resolve => {
-    var m: RegExpMatchArray = i.match(r);
-    resolve(r.source + " " + (m ? m.length : 0));
-}));
+  const q: RegExp[] = [
+    /agggtaaa|tttaccct/gi,
+    /[cgt]gggtaaa|tttaccc[acg]/gi,
+    /a[act]ggtaaa|tttacc[agt]t/gi,
+    /ag[act]gtaaa|tttac[agt]ct/gi,
+    /agg[act]taaa|ttta[agt]cct/gi,
+    /aggg[acg]aaa|ttt[cgt]ccct/gi,
+    /agggt[cgt]aa|tt[acg]accct/gi,
+    /agggta[cgt]a|t[acg]taccct/gi,
+    /agggtaa[cgt]|[acg]ttaccct/gi,
+  ];
 
-async function displayOutput(promises: Promise<string>[], ilen: number,
-        clen: number, iubReplaceLen: Promise<number>): Promise<void> {
-    for (var count = 0; count < 9; count++)
-        console.log(await promises[count]);
+  const promises: Promise<string>[] = q.map(
+    (r) =>
+      new Promise<string>((resolve) => {
+        const m: RegExpMatchArray | null = i.match(r);
+        resolve(r.source + " " + (m ? m.length : 0));
+      })
+  );
 
-    console.log();
-    console.log(ilen);
-    console.log(clen);
-    console.log(await iubReplaceLen);
+  // Output results
+  for (let count = 0; count < promises.length; count++)
+    console.log(await promises[count]);
+
+  console.log();
+  console.log(ilen);
+  console.log(clen);
+  console.log(await iubReplaceLen);
 }
 
-displayOutput(promises, ilen, clen, iubReplaceLen);
+// DENO MODIFICATION: Call the main function with error handling
+main().catch((err) => {
+  console.error("Error:", err);
+  Deno.exit(1);
+});
